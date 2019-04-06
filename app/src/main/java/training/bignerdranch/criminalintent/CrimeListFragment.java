5f75098303327150cf3572f827b719e3e0d17bbe
Trettende <compdolblu@gmail.com>
2019-04-06 17:57:20 +0300

    chapter9 challenge commit

--- app/src/main/java/training/bignerdranch/criminalintent/CrimeListFragment.java
+++ app/src/main/java/training/bignerdranch/criminalintent/CrimeListFragment.java
@@ -13,7 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class CrimeListFragment extends Fragment {
 
@@ -60,7 +62,9 @@ public class CrimeListFragment extends Fragment {
         public void bind(Crime crime) {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
-            mDateTextView.setText(mCrime.getDate().toString());
+            String date = DateFormat.getDateInstance(DateFormat.FULL, Locale.ENGLISH)
+                    .format(mCrime.getDate());
+            mDateTextView.setText(date);
             mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
